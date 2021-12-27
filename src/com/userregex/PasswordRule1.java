47a07b92a0bab6e5a7f325760d5027e8a882e54a
Priya412-coder <priyankasingh8249@gmail.com>
2021-12-27 09:59:28 +0530

    adding UC5 :Rule1 – minimum 8 Characters

---
+++ src/com/userregex/PasswordRule1.java
@@ -0,0 +1,28 @@
+package com.userregex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRule1 {
+	public boolean passwrdRule(String inputStr) {
+		String passwordPattern = "^[A-Za-z]{8}$";
+		Pattern regex = Pattern.compile(passwordPattern);
+		Matcher passwordMatcher = regex.matcher(inputStr);
+		if (passwordMatcher.matches()) {
+			System.out.println(inputStr + " succesfully loggedin and passed first rule.");
+		} else {
+			System.out.println(inputStr + " is invalid password.Kindly input minimum 8 character.");
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		PasswordRule1 psswrd = new PasswordRule1();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the password : ");
+		String inputStr = sc.nextLine();
+		sc.close();
+		psswrd.passwrdRule(inputStr);
+	}
+
+}
